360f05afb3780a7dc33979d1a5393c22a6340063
Evangelos Giataganas <e.giataganas@gmail.com>
2022-04-04 17:18:16 +0300

    Add beta feedback flag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  FEEDBACK_EMAILS = {
+    lambeth: "digitalplanning@lambeth.gov.uk",
+    southwark: "digital.projects@southwark.gov.uk",
+    buckinghamshire: "planning.digital@buckinghamshire.gov.uk",
+  }.freeze
+
+  def feedback_email(current_local_authority)
+    FEEDBACK_EMAILS[current_local_authority.to_sym]
+  end
 end
