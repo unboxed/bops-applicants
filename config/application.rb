1d59f57a29791c69178f6c5a30c4ceebd2ad0313
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-02-09 17:19:46 +0000

    Upgrade to Rails 7.0

--- config/application.rb
+++ config/application.rb
@@ -3,11 +3,11 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 # require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module BopsApplicants
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -34,8 +34,6 @@ module BopsApplicants
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.autoload_paths += %W[#{config.root}/lib]
 
     config.action_controller.forgery_protection_origin_check = false
   end
