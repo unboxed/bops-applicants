75b9533a7f2765981543f04ec089c897d4df5d70
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-02-14 11:00:34 +0000

    Remove UJS and Turbolinks, which are not being used

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@rails/ujs").start()
-require("turbolinks").start()
+// require("@rails/ujs").start()
+// require("turbolinks").start()
 
 import "@opensystemslab/map"
 // Uncomment to copy all static images under ../images to the output folder and reference
