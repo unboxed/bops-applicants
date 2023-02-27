d97efe3c77cb768460fb8a3cadfb478fd0c31835
Ben <benbaumann95@hotmail.co.uk>
2023-02-27 11:09:06 +0000

    Add feedback fish widget
    - Send user's email in feedback request
    - Split pid id per environment so we can have a specific project for staging / production
    - https://github.com/unboxed/bops/pull/947

--- spec/system/landing_page_spec.rb
+++ spec/system/landing_page_spec.rb
@@ -24,8 +24,42 @@ RSpec.describe "landing page", type: :system do
   end
 
   context "within phase-banner" do
-    it "lets the user navigate to feedback" do
-      within(".govuk-phase-banner") { expect(page).to have_link("feedback") }
+    context "when FEEDBACK_FISH_ID env variable is set" do
+      before do
+        ENV["FEEDBACK_FISH_ID"] = "randomid123"
+
+        visit(
+          validation_requests_path(
+            planning_application_id: 28,
+            change_access_id: 345_443_543
+          )
+        )
+      end
+
+      it "is displayed with a reference to the user's email" do
+        within find("a[data-feedback-fish][data-feedback-fish-userid='agent@example.com']") do
+          expect(page).to have_content "feedback"
+        end
+      end
+    end
+
+    context "when FEEDBACK_FISH_ID env variable is not set" do
+      before do
+        ENV["FEEDBACK_FISH_ID"] = nil
+
+        visit(
+          validation_requests_path(
+            planning_application_id: 28,
+            change_access_id: 345_443_543
+          )
+        )
+      end
+
+      it "is not displayed" do
+        within(".govuk-phase-banner__content") do
+          expect(page).not_to have_content "feedback"
+        end
+      end
     end
   end
 
