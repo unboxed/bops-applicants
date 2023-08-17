d178ebd26926d1c42e60b7c9cdaebcf368d79438
Benjamin Baumann <benjaminbaumann@Ben-Baumanns-MacBook-Pro-C02FM2C8MD6N.local>
2023-08-17 11:34:47 +0100

    Update docker-entrypoint and set bundle path
    Related to https://github.com/unboxed/bops/pull/1230

--- docker/ruby/docker-entrypoint.sh
+++ docker/ruby/docker-entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 set -e
+echo "Initializing BOPS applicants"
+
+echo "Installing yarn"
+yarn install
+
+export BUNDLE_PATH=/home/rails/bundle
+echo "Bundling gems"
+bundle check || bundle install
 
 if [ "$1" = "bash" ]; then
   exec "$@"
 else
-  yarn install
-  bundle check || bundle install
-
   bundle exec "$@"
 fi
