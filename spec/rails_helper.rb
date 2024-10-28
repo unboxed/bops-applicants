f79198c16f6d1a35883b0cc12b6f0da9f158ba18
Andrew White <andrew.white@unboxed.co>
2024-10-28 11:49:13 +0000

    Ensure migrations are applied before running specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 
@@ -10,6 +9,13 @@ require "rspec/rails"
 require "webmock/rspec"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 
 RSpec.configure do |config|
   config.file_fixture_path = Rails.root.join("spec/fixtures")
